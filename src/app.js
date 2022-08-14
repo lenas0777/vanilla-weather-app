fb0908406ce8cd538437c686ada6429fb3627821
lenas0777 <110209619+lenas0777@users.noreply.github.com>
2022-08-14 21:08:26 +0300

    Updated icon element

--- src/app.js
+++ src/app.js
@@ -14,22 +14,28 @@ let day=days[date.getDay()];
 
 }
 function displayTemperature(response) {
+    console.log(response.data);
     let temperatureElement=document.querySelector("#temperature");
     let cityElement=document.querySelector("#city");
     let descriptionElement=document.querySelector("#description");
     let humidityElement=document.querySelector("#humidity");
     let windElement=document.querySelector("#wind");
     let dateElement=document.querySelector("#date");
+    let iconElement=document.querySelector("#icon");
+    let weatherCond=response.data.weather[0].icon;
     temperatureElement.innerHTML=Math.round(response.data.main.temp);
     cityElement.innerHTML=response.data.name;
     descriptionElement.innerHTML=response.data.weather[0].description;
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${weatherCond}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 humidityElement.innerHTML=response.data.main.humidity;
 windElement.innerHTML=`${response.data.wind.speed} km/h`;
 dateElement.innerHTML= formatDate(response.data.dt*1000);
 }
 
 let apiKey="7644cf0e41cb2d8fdabc6b0808cee422";
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city="Sydney";
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 
 axios.get(apiUrl).then(displayTemperature);
