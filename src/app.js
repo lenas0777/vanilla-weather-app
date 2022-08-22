c3c033ff748e3d17d3e8a58a1cea7fdb9e2f929c
lenas0777 <lenas0777@gmail.com>
2022-08-22 17:28:01 +0300

    Integrated API and injected the data to forecast

--- src/app.js
+++ src/app.js
@@ -21,26 +21,39 @@ function formatDate(timestamp) {
   return `${day}, ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
+
   let forecastHTML = `<div class="row">`;
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6)
+      forecastHTML =
+        forecastHTML +
+        `
                           <div class="col-2">
                             <div class="weather-forecast-date">
-                                ${day}
+                                ${formatDay(forecastDay.dt)}
                             </div>
-                            <img src="http://openweathermap.org/img/wn/02n@2x.png" alt="" width="42" id="img" />
+                            <img src="http://openweathermap.org/img/wn/${
+                              forecastDay.weather[0].icon
+                            }.png" alt="" width="42" id="img" />
                             <div class="weather-forecast-temperatures">
                                 <span class="weather-forecast-temperature-max">
-                                    18°
+                                    ${Math.round(forecastDay.temp.max)}
                                 </span>
                                 <span class="weather-forecast-temperature-min">
-                                    12°
+                                    ${Math.round(forecastDay.temp.min)}
                                 </span>
                             </div>
                     </div>
